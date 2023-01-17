bef66b3743d3d33ae359d632c8ea6a079d88d876
v.ilyukevich <v.ilyukevich@rit.va>
2023-01-17 15:41:21 +0300

    not ending transition

--- src/componets/PostList.vue
+++ src/componets/PostList.vue
@@ -2,12 +2,15 @@
   <div v-if="posts.length > 0">
     <hr class="separator">
     <h2 class="title">Список постов</h2>
+    <transition-group name="post-list">
       <post-item
           v-for="post in posts"
           :post="post"
           :key="post.id"
           @remove="$emit('remove', post)"
       />
+    </transition-group>
+
   </div>
   <div v-else>
     <hr class="separator">
